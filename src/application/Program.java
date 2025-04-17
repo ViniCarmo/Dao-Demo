cf0b9714aab137d861f06d5fe28f2515ab96d31d
ViniCarmo <vinicius.carmo1106@gmail.com>
2025-04-17 18:11:53 -0300

    Insert implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -32,5 +32,11 @@ public class Program {
         for (Seller obj : list){
             System.out.println(obj);
         }
+
+        System.out.println();
+        System.out.println("=== TEST 4: Seller Insert ===");
+        Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0, department);
+        sellerDao.insert(newSeller);
+        System.out.println("Inserted, new id = " + newSeller.getId());
     }
 }
